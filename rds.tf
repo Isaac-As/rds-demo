cc98c0567623d404bd2590e6f211931b22d9aa5c
Isaac A <isaac.l.asumani@gmail.com>
2023-11-24 09:13:08 -0500

    my first commit

---
+++ rds.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIAR4YVLLLHXH6XQBWL"
+    secret_key = "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"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
